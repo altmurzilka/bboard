016fcabc6f7b8bf67a63126f3cfa865a0e99d700
--replace-all <altmurzilka@gmail.com>
2020-03-18 02:30:40 +0600

    adding files

---
+++ bboard/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Bb
+
+admin.site.register(Bb)
+
+# Register your models here.
---
+++ bboard/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Bb 
+
+def index(request):
+    bbs = Bb.object.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs' : bbs})
+
+# Create your views here.
