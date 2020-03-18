ed347f7086e39aaa18ee585d626b5142f0c655dc
altmurzilka <altmurzilka@gmail.com>
2020-03-18 17:17:22 +0600

    "some update"

--- bboard/urls.py
+++ bboard/urls.py
@@ -4,6 +4,6 @@ from .views import index
 from .views import by_rubric
 
 urlpatterns = [
-    path('<int:rubric_id>/', by_rubric),
-    path('', index),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('', index, name='index'),
 ]
