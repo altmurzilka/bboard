e3774f0c23b32119e412cb9eb5ca2d3aebf6875c
altmurzilka <altmurzilka@gmail.com>
2020-03-18 11:21:02 +0600

    "lgfm"

--- bboard/models.py
+++ bboard/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Bb(models.Model):
-    title = models.CharField(max_length = 50)
-    content = models.TextField(null = True, blank = True)
-    price = models.FloatField(null=True, blank = True)
-    published = models.DateTimeField(auto_now_add = True, db_index = True)
+    title = models.CharField(max_length=50, verbose_name="Item")
+    content = models.TextField(
+        null=True, blank=True, verbose_name="Description")
+    price = models.FloatField(null=True, blank=True, verbose_name='Price')
+    published = models.DateTimeField(
+        auto_now_add=True, db_index=True, verbose_name='Published')
+
+
+class Meta:
+    verbose_name_plural = 'Notes'
+    verbose_name = 'Note'
+    ordering = ['-published']
