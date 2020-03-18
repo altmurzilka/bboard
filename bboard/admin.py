2ddccc1732395e1c9a6a9a6d2eda12d5e1cf7759
altmurzilka <altmurzilka@gmail.com>
2020-03-18 11:59:53 +0600

    "fix view bug from db"

--- bboard/admin.py
+++ bboard/admin.py
@@ -5,7 +5,7 @@ from .models import Bb
 class BbAdmin(admin.ModelAdmin):
     list_display = ('title', 'content', 'price', 'published')
     list_display_links = ('title', 'content')
-    search_fields = ('title', 'content')
+    search_fields = ('title', 'content',)
 
 admin.site.register(Bb)
 
--- bboard/models.py
+++ bboard/models.py
@@ -5,16 +5,15 @@ from django.db import models
 
 class Bb(models.Model):
     title = models.CharField(max_length=50, verbose_name="Item")
-    content = models.TextField(
-        null=True, blank=True, verbose_name="Description")
+    content = models.TextField(null=True, blank=True, verbose_name="Description")
     price = models.FloatField(null=True, blank=True, verbose_name='Price')
-    published = models.DateTimeField(
-        auto_now_add=True, db_index=True, verbose_name='Published')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Published')
 
-    objects = models.Manager()
+    # objects = models.Manager()
+    class Meta:
+        verbose_name_plural = 'Notes'
+        verbose_name = 'Note'
+        ordering = ['-published']
 
 
-class Meta:
-    verbose_name_plural = 'Notes'
-    verbose_name = 'Note'
-    ordering = ['-published']
+
